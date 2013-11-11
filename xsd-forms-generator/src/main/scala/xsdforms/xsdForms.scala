3463bfcf268bda39bebc8f0255660b8071eb5fb1
Dave Moten <davidmoten@gmail.com>
2013-11-12 07:40:15 +1100

    look at validation of unselected radio button

--- xsd-forms-generator/src/main/scala/xsdforms/xsdForms.scala
+++ xsd-forms-generator/src/main/scala/xsdforms/xsdForms.scala
@@ -1106,7 +1106,7 @@ package xsdforms {
     private def createEnumerationTestScriptlet(node: NodeBasic) = {
       if (isEnumeration(restriction(node))) {
         "\n|  //enumeration test" +
-          "\n|  if (v.val().length ==0) ok = false;"
+          "\n|  if ((typeof(v.val()) != 'undefined') && (v.val().length ==0)) ok = false;"
       } else
         ""
     }
