ee4af56b59b9bd8ca15ae2790a7ea5b614faa519
Dave Moten <davidmoten@gmail.com>
2013-12-25 22:11:16 +1030

    add scaladocs

--- xsd-forms-generator/update-samples.sh
+++ xsd-forms-generator/update-samples.sh
@@ -3,4 +3,8 @@ set -e
 rm -rf src/docs/showcase
 mkdir src/docs/showcase
 cp -r target/generated-webapp/* src/docs/showcase
+mvn scala:doc
+rm -rf src/docs/scaladocs
+mkdir src/docs/scaladocs
+cp -r target/site/scaladocs/* src/docs/scaladocs
 
