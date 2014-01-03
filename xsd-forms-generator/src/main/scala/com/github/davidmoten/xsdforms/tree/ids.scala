1b48af548798335ec574438533ce4dbf83794e93
Dave Moten <davidmoten@gmail.com>
2014-01-03 18:05:48 +1100

    move Ids to own file

---
+++ xsd-forms-generator/src/main/scala/com/github/davidmoten/xsdforms/tree/ids.scala
@@ -0,0 +1,55 @@
+package com.github.davidmoten.xsdforms.tree
+
+object Ids {
+
+  val InstanceDelimiter = "-instance-"
+  val ChoiceIndexDelimiter = "-choice-"
+
+  def getItemId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
+    idPrefix + "item-" + number + InstanceDelimiter + instances
+
+  def getItemName(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "item-input-" + number + InstanceDelimiter + instances;
+
+  def getItemErrorId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "item-error-" + number + InstanceDelimiter + instances
+
+  def getChoiceItemId(number: Int, index: Int,
+    instances: Instances)(implicit idPrefix: Prefix): String =
+    getItemId(number, instances)(idPrefix) + ChoiceIndexDelimiter + index
+
+  def getChoiceItemName(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "item-input-" + number + InstanceDelimiter + instances
+
+  def choiceContentId(number: Int, index: Int,
+    instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "choice-content-" + number + InstanceDelimiter +
+      instances + ChoiceIndexDelimiter + index
+
+  def getItemId(number: Int, enumeration: Integer,
+    instances: Instances)(implicit idPrefix: Prefix): String =
+    getItemId(number, instances) + "-" + enumeration
+
+  def getRepeatButtonId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "repeat-button-" + number + InstanceDelimiter + instances
+
+  def getRemoveButtonId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "remove-button-" + number + InstanceDelimiter + instances
+
+  def getRepeatingEnclosingId(number: Int,
+    instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "repeating-enclosing-" + number + InstanceDelimiter + instances
+
+  def getMinOccursZeroId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "min-occurs-zero-" + number + InstanceDelimiter + instances
+
+  def getMinOccursZeroName(number: Int,
+    instances: Instances)(implicit idPrefix: Prefix): String =
+    idPrefix + "min-occurs-zero-name" + number + InstanceDelimiter + instances
+
+  def getItemEnclosingId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
+    idPrefix + "item-enclosing-" + number + InstanceDelimiter + instances
+
+  def getPathId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
+    idPrefix + "item-path-" + number + InstanceDelimiter + instances
+}
--- xsd-forms-generator/src/main/scala/com/github/davidmoten/xsdforms/tree/tree-util.scala
+++ xsd-forms-generator/src/main/scala/com/github/davidmoten/xsdforms/tree/tree-util.scala
@@ -33,61 +33,6 @@ protected case class HtmlJs(html: Vector[String], js: Vector[String]) {
   def addJs(js2: String) = HtmlJs(html, js.+:(js2))
 }
 
-object Ids {
-
-  val InstanceDelimiter = "-instance-"
-  val ChoiceIndexDelimiter = "-choice-"
-
-  def getItemId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
-    idPrefix + "item-" + number + InstanceDelimiter + instances
-
-  def getItemName(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "item-input-" + number + InstanceDelimiter + instances;
-
-  def getItemErrorId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "item-error-" + number + InstanceDelimiter + instances
-
-  def getChoiceItemId(number: Int, index: Int,
-    instances: Instances)(implicit idPrefix: Prefix): String =
-    getItemId(number, instances)(idPrefix) + ChoiceIndexDelimiter + index
-
-  def getChoiceItemName(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "item-input-" + number + InstanceDelimiter + instances
-
-  def choiceContentId(number: Int, index: Int,
-    instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "choice-content-" + number + InstanceDelimiter +
-      instances + ChoiceIndexDelimiter + index
-
-def getItemId(number: Int, enumeration: Integer,
-    instances: Instances)(implicit idPrefix:Prefix): String =
-    getItemId(number, instances) + "-" + enumeration
-
-      
-  def getRepeatButtonId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "repeat-button-" + number + InstanceDelimiter + instances
-
-  def getRemoveButtonId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "remove-button-" + number + InstanceDelimiter + instances
-
-  def getRepeatingEnclosingId(number: Int,
-    instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "repeating-enclosing-" + number + InstanceDelimiter + instances
-
-  def getMinOccursZeroId(number: Int, instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "min-occurs-zero-" + number + InstanceDelimiter + instances
-
-  def getMinOccursZeroName(number: Int,
-    instances: Instances)(implicit idPrefix: Prefix): String =
-    idPrefix + "min-occurs-zero-name" + number + InstanceDelimiter + instances
-
-  def getItemEnclosingId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
-    idPrefix + "item-enclosing-" + number + InstanceDelimiter + instances
-
-  def getPathId(number: Int, instances: Instances)(implicit idPrefix: Prefix) =
-    idPrefix + "item-path-" + number + InstanceDelimiter + instances
-}
-
 protected trait TreeState {
   import com.github.davidmoten.xsdforms.html.Html
 
